477ebc7c8260a2b21325b39bcf884ad5417cecc1
numcayX2 <sokulshop5@gmail.com>
2021-10-09 17:41:12 +0700

    Added new files

---
+++ scrip.js
@@ -0,0 +1,46 @@
+function Lia() {
+    document.getElementById("paragraph").innerHTML = "Name : Mitsuki Lia 【WACTOR】";
+
+    var pic = "Cpicture/Lia.png"
+    document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
+    document.getElementById('bigpic').style.display='block';
+}
+
+function Piyoko() {
+    document.getElementById("paragraph").innerHTML = "Name : Kogure Piyoko【WACTOR】"
+
+    var pic = "Cpicture/Piyoko.jpg"
+    document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
+    document.getElementById('bigpic').style.display='block';
+}
+
+function Hana() {
+    document.getElementById("paragraph").innerHTML = "Name : Tsukito Hana【WACTOR】"
+}
+
+function Laila() {
+    document.getElementById("paragraph").innerHTML = "Name : Shino Laila【WACTOR】"
+}
+
+function Suzu() {
+    document.getElementById("paragraph").innerHTML = "Name : Hoshina Suzu【WACTOR】"
+}
+
+function Rose() {
+    document.getElementById("paragraph").innerHTML = "Name : Kurari Rose【WACTOR】"
+}
+
+function Hina() {
+    document.getElementById("paragraph").innerHTML = "Name : Misora Hina【WACTOR】"
+}
+
+function Pal() {
+    document.getElementById("paragraph").innerHTML = "Name : Sumeragi Pal【WACTOR】"
+}
+
+function Luna() {
+    document.getElementById("paragraph").innerHTML = "Name : Rurine Luna【WACTOR】"
+}
+
+//img
+
